641bcab35c1269352d404697e140521232501eea
Robin Steinhauser <rbn2208@gmail.com>
2021-01-29 11:56:52 +0100

    changed review tips

--- src/js/bookmarktoggle.js
+++ src/js/bookmarktoggle.js
@@ -1,8 +1,6 @@
 const bookmarkSigns = document.querySelectorAll('[data-js="bookmarkSign"]')
-console.log(bookmarkSigns)
 
 bookmarkSigns.forEach(sign => {
-  //const button = document.querySelector('[data-js="bookmarkSign"]')
   sign.addEventListener('click', () => {
     sign.classList.toggle('marked')
   })

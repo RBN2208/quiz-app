a22e0680d6a09f4239f2fb925cc02a8f0fa41588
Robin Steinhauser <rbn2208@gmail.com>
2021-01-29 12:50:00 +0100

    added counter for textareas

--- src/js/createform.js
+++ src/js/createform.js
@@ -1,13 +1,32 @@
 const formEl = getElement('formElement')
-const formQuestion = getElement('formQuestion')
-const formAnswer = getElement('formAnswer')
-const formTags = getElement('formTags')
+const textareaQuestion = getElement('formQuestion')
+const textareaAnswer = getElement('formAnswer')
+const textareaTags = getElement('formTags')
+
+const questionCounter = getElement('qCounter')
+const answerCounter = getElement('aCounter')
+const tagCounters = getElement('tagCounter')
+
+textareaQuestion.addEventListener('input', event => {
+  const qcounter = document.querySelector('[data-js="qCounter"]')
+  qcounter.textContent = `${event.currentTarget.value.length}/200`
+})
+
+textareaAnswer.addEventListener('input', event => {
+  const acounter = document.querySelector('[data-js="aCounter"]')
+  acounter.textContent = `${event.currentTarget.value.length}/200`
+})
+
+textareaTags.addEventListener('input', event => {
+  const tcounter = document.querySelector('[data-js="tagCounter"]')
+  tcounter.textContent = `${event.currentTarget.value.length}/50`
+})
 
 formEl.addEventListener('submit', event => {
   event.preventDefault()
   console.log(event)
   formEl.reset()
-  formQuestion.focus()
+  textareaQuestion.focus()
 })
 
 function getElement(dataJsName) {

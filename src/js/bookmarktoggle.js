6e48c7c8e9bac33c3351bb5e370a879c1bb87922
Robin Steinhauser <rbn2208@gmail.com>
2021-01-29 16:19:41 +0100

    merged js-files into index.js

--- src/js/bookmarktoggle.js
+++
@@ -1,7 +0,0 @@
-const bookmarkSigns = document.querySelectorAll('[data-js="bookmarkSign"]')
-
-bookmarkSigns.forEach(sign => {
-  sign.addEventListener('click', () => {
-    sign.classList.toggle('marked')
-  })
-})
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
+// constanten
+const cards = document.querySelectorAll('[data-js="cards"]')
+
 const formEl = getElement('formElement')
 const textareaQuestion = getElement('inputbox')
-
 const labels = document.querySelectorAll('[data-js="inputLabels"]')
+
+const bookmarkSigns = document.querySelectorAll('[data-js="bookmarkSign"]')
+
+// functions
+cards.forEach(card => {
+  const button = card.querySelector('[data-js="answerButton"]')
+  const answer = card.querySelector('[data-js="answerBox"]')
+
+  button.addEventListener('click', () => {
+    answer.classList.toggle('hidden')
+  })
+})
 
 labels.forEach(label => {
   const textarea = label.querySelector('[data-js="inputbox"]')
@@ -17,6 +31,14 @@ formEl.addEventListener('submit', event => {
   formEl.reset()
   textareaQuestion.focus()
 })
+
+bookmarkSigns.forEach(sign => {
+  sign.addEventListener('click', () => {
+    sign.classList.toggle('marked')
+  })
+})
+
+// function for data-js import
 
 function getElement(dataJsName) {
   return document.querySelector(`[data-js="${dataJsName}"]`)
